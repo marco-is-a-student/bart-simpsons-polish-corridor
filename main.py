847b29dd807fe01b5111225617b10071aa7d9167
marco-is-a-student <alvarez_marco_lorenzo_tanzon@s2023.ssts.edu.sg>
2026-01-07 16:32:37 +0800

    added 'features'

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ st.set_page_config(page_title="Avenger's doomsday countdown",page_icon="🤖",la
 
 if 'character' not in st.session_state:
     st.session_state.character = "67 kid"
+if 'Avengers Doomsday' not in st.session_state:
+    st.session_state.movie = "Avengers Doomsday"
 st.markdown(f"""
 <h1 style="font-family: 'Times New Roman'; text-align: center">
-avenger's doomsday countdown with {st.session_state.character}
+{st.session_state.movie} countdown with {st.session_state.character}
 </h1>
 """
 ,unsafe_allow_html=True)
@@ -15,6 +17,11 @@ avenger's doomsday countdown with {st.session_state.character}
 col1,col2,col3,col4,col5 = st.columns(5)
 with col2:
     st.selectbox("Character",["67 kid","Wario","animegirl1","animegirl2","bart simpson","Frank"],key="character",width=200)
+with col4:
+    st.selectbox("Movie",["Avengers Doomsday","thats all"],key="movie",width=200)
+    toasty = st.button("click here to toast")
+    if toasty:
+        st.toast("### HIIIIII",duration="short")
 with col3:
     if st.session_state.character == "67 kid":
         st.image("./images/67kid.jpeg")
