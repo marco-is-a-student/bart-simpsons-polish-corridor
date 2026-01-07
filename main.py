52db44b3a56dcfd722d4a29bc2a51e36b72e2f89
marco-is-a-student <alvarez_marco_lorenzo_tanzon@s2023.ssts.edu.sg>
2026-01-07 16:22:48 +0800

    fixed image sizes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+from PIL import Image
 st.set_page_config(page_title="Avenger's doomsday countdown",page_icon="🤖",layout="wide")
 
 if 'character' not in st.session_state:
@@ -11,6 +11,26 @@ avenger's doomsday countdown with {st.session_state.character}
 """
 ,unsafe_allow_html=True)
 
+
 col1,col2,col3,col4,col5 = st.columns(5)
 with col2:
-    st.selectbox("Character",["67 kid","Wario","animegirl1","animegirl2"],key="character",width=200)
+    st.selectbox("Character",["67 kid","Wario","animegirl1","animegirl2","bart simpson","Frank"],key="character",width=200)
+with col3:
+    if st.session_state.character == "67 kid":
+        st.image("./images/67kid.jpeg")
+    elif st.session_state.character == "Wario":
+        img2 = Image.open("./images/Wario.png")
+        resized = img2.resize((300,150))
+        st.image(resized)
+    elif st.session_state.character == "animegirl1":
+        st.image("./images/animegirl1.jpg")
+    elif st.session_state.character == "animegirl2":
+        img2 = Image.open("./images/animegirl2.webp")
+        resized = img2.resize((200,350))
+        st.image(resized)
+    elif st.session_state.character == "bart simpson":
+        img2 = Image.open("./images/emobart.jpg")
+        resized = img2.resize((200,350))
+        st.image(resized)
+    elif st.session_state.character == "Frank":
+        st.image("./images/IMG_2373.jpg")
